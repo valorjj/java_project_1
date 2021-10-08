9405387dfe34d5cae24bb0601829631aa4faa390
505 <505@DESKTOP-B9HIA7V>
2021-10-08 15:08:31 +0900

    1

--- project/src/View/main.java
+++ project/src/View/main.java
@@ -6,16 +6,10 @@ import Domain.*;
 
 public class main {
 
-	static String[][] coffee_menu = new String[100][3]; // 메뉴 이름, 메뉴 가격, 메뉴 재고 
-	static Scanner scanner = new Scanner(System.in);
-	// 1. 메인 메소드 
+
 	public static void main(String[] args) {
 		
-		// Domain 패키지에 있는 클래스들을 호출 
-		
-		admin_page admin_menu = new admin_page();
-		
-		
+		System.out.println("이 페이지는 사용하지 않습니다. ");
 		
 
 	}
